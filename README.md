8e3406ada5d3b903aed6567da9a1b0d9a8c715f8
Haijin Yan <life0fun@gmail.com>
2014-03-10 21:48:07 -0700

    init

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-minuette
-========
+# Minuette
 
-A RESTFul scheduling service using on Quartz 
+Minuette is a RESTFul scheduling service using Quartz 
+
+Minuette depends on Quartzite, a clojure scheduling library which built on top of Quartz Scheduler to implement scheduling.
+
+
+## Configuration and Run
+
+1. lein new pedestal-service minuette
+
+2. Start the service: `lein run-dev` \*
+3. Go to [localhost:8080](http://localhost:8080/).
+
+4. config/logback.xml config By default, the app logs to stdout and logs/ 
+
+5. curl, or use request python lib to send post requests to create schedules or update, query schedules.
+
+
+## Quartz Lib and Quartzite
+
+Minuette utilize Quartzite lib that is based on the Quartz Scheduler to support for sophisticated schedules, multiple calendars, Cron expressions, clustering, plugins and more.
+
+Minuette allows manage operation and schedules via RESTful APIs. It also has web app that support query and visualize schedules. Minuette persistents schedules into mysql db to preserve state across crashes. 
+
+##
+
---
+++ project.clj
@@ -0,0 +1,31 @@
+(defproject minuette "0.0.1-SNAPSHOT"
+  :description "a scheduling service"
+  :url "http://minuette.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [io.pedestal/pedestal.service "0.2.2"]
+                 [io.pedestal/pedestal.service-tools "0.2.2"]
+
+                 ;; Remove this line and uncomment the next line to
+                 ;; use Tomcat instead of Jetty:
+                 [io.pedestal/pedestal.jetty "0.2.0"]
+                 ;; [io.pedestal/pedestal.tomcat "0.2.2"]
+
+                 [clojurewerkz/quartzite "1.1.0"]
+                 ]
+  :min-lein-version "2.0.0"
+  :resource-paths ["config", "resources"]
+  :aliases {"run-dev" ["trampoline" "run" "-m" "minuette.server/run-dev"]}
+  :repl-options  {:init-ns user
+                  :init (try
+                          (use 'io.pedestal.service-tools.dev)
+                          (require 'minuette.service)
+                          ;; Nasty trick to get around being unable to reference non-clojure.core symbols in :init
+                          (eval '(init minuette.service/service #'minuette.service/routes))
+                          (catch Throwable t
+                            (println "ERROR: There was a problem loading io.pedestal.service-tools.dev")
+                            (clojure.stacktrace/print-stack-trace t)
+                            (println)))
+                  :welcome (println "Welcome to pedestal-service! Run (tools-help) to see a list of useful functions.")}
+  :main ^{:skip-aot true} minuette.server)
